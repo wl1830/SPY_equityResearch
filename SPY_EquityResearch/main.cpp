0b5230bb2643885c606c56761a04fcf42da35a6f
Weitingting Liu <wl1830@nyu.edu>
2019-12-02 07:59:27 -0500

    Initial Commit

---
+++ SPY_EquityResearch/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SPY_EquityResearch
+//
+//  Created by 刘唯婷婷 on 12/2/19.
+//  Copyright © 2019 NYU. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
